9b56b686bad810d5a61207f354bf94a3d6fb2c18
Deepikavasanthi <deepikamailtome@gmail.com>
2016-08-07 23:45:02 -0700

    Add files via upload

---
+++ dayequal.java
@@ -0,0 +1,8 @@
+String s;
+      Scanner sc=new Scanner(System.in);
+      s=sc.nextLine();
+      if(s.equalsIgnoreCase("sunday"))
+       System.out.println("true");
+      else
+        System.out.println("false");
+      
